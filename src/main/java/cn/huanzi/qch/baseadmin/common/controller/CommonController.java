f8d63c83f7f4b7cb98e1406628efe5ea9f87a0e2
zhu <25092197@qq.com>
2020-09-23 16:40:01 +0800

    Revert "1、新增若干常见问题"
    This reverts commit 91b653a9

--- src/main/java/cn/huanzi/qch/baseadmin/common/controller/CommonController.java
+++ src/main/java/cn/huanzi/qch/baseadmin/common/controller/CommonController.java
@@ -22,7 +22,7 @@ import java.util.List;
  */
 public class CommonController<V, E, T> {
 
-    @Autowired(required = false)
+    @Autowired
     private CommonService<V, E, T> commonService;
 
     /*
